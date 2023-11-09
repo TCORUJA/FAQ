c3035a728c4341aa02bf52a0ddfa6f2fa9bb8c20
TCORUJA <113937074+TCORUJA@users.noreply.github.com>
2023-11-09 10:17:29 -0300

    Add files via upload

--- PosGraduacao/EAD/script.js
+++ PosGraduacao/EAD/script.js
@@ -109,3 +109,287 @@ function botaoExpandirRecolherPergunta6(){
         document.getElementById('botaoExpadirRecolherPergunta6').setAttribute("name", "expandir");
     }
 }
+
+function botaoExpandirRecolherPergunta7(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta7').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta7').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta7').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta7').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta7').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta7').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta7').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta7').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta7').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta7').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta7').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta7').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta8(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta8').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta8').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta8').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta8').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta8').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta8').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta8').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta8').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta8').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta8').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta8').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta8').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta9(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta9').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta9').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta9').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta9').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta9').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta9').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta9').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta9').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta9').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta9').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta9').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta9').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta10(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta10').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta10').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta10').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta10').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta10').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta10').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta10').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta10').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta10').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta10').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta10').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta10').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta11(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta11').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta11').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta11').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta11').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta11').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta11').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta11').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta11').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta11').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta11').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta11').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta11').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta12(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta12').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta12').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta12').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta12').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta12').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta12').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta12').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta12').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta12').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta12').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta12').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta12').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta13(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta13').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta13').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta13').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta13').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta13').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta13').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta13').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta13').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta13').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta13').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta13').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta13').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta14(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta14').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta14').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta14').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta14').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta14').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta14').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta14').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta14').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta14').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta14').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta14').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta14').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta15(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta15').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta15').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta15').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta15').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta15').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta15').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta15').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta15').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta15').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta15').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta15').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta15').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta16(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta16').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta16').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta16').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta16').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta16').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta16').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta16').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta16').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta16').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta16').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta16').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta16').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta17(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta17').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta17').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta17').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta17').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta17').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta17').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta17').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta17').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta17').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta17').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta17').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta17').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta18(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta18').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta18').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta18').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta18').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta18').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta18').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta18').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta18').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta18').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta18').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta18').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta18').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta19(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta19').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta19').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta19').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta19').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta19').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta19').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta19').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta19').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta19').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta19').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta19').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta19').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta20(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta20').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta20').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta20').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta20').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta20').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta20').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta20').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta20').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta20').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta20').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta20').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta20').setAttribute("name", "expandir");
+    }
+}
+
+
+function botaoExpandirRecolherPergunta21(){
+
+    if(document.getElementById('botaoExpadirRecolherPergunta21').getAttribute("name") == "expandir"){
+        document.getElementById('conteudoRespostaPergunta21').classList.remove('ocultar');
+        document.getElementById('conteudoRespostaPergunta21').classList.add('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta21').classList.remove('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta21').classList.add('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta21').setAttribute("name", "recolher");
+    }
+    else if(document.getElementById('botaoExpadirRecolherPergunta21').getAttribute("name") == "recolher"){
+        document.getElementById('conteudoRespostaPergunta21').classList.add('ocultar');
+        document.getElementById('conteudoRespostaPergunta21').classList.remove('exibir');
+        document.getElementById('botaoExpadirRecolherPergunta21').classList.remove('recolher');
+        document.getElementById('botaoExpadirRecolherPergunta21').classList.add('expandir');
+        document.getElementById('botaoExpadirRecolherPergunta21').setAttribute("name", "expandir");
+    }
+}
